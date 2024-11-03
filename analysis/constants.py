37a774a82b82273c19644a19ffac1434b55a0d5d
RussoLucas <lukasrusso22@gmail.com>
2024-11-03 15:13:26 -0700

    Finishing initial version of overview analysis

---
+++ analysis/constants.py
@@ -0,0 +1,3 @@
+GROUPS_TO_INCLUDE = ['All', 'White', 'Black', 'Hispanic', 'Asian', 'Native']
+PATH22 = "../AVT-Proficiency-SY-2021-22-Cleaned.xlsx"
+PATH23 = "../AVT-Proficiency-SY-2022-23-Cleaned.xlsx"
